9cf93a25941f3fb64c7a0791a4d36fc0e56d89a2
goh-chunlin <chunlin@live.com.sg>
2020-05-04 14:54:28 +0800

    Initial Commit

---
+++ geolocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  geolocation
+//
+//  Created by Goh Chun Lin on 4/5/20.
+//  Copyright © 2020 GCL Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
