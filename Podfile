a3dbfb166bf76eff54c28f4dc0dda3836ec7505d
goh-chunlin <chunlin@live.com.sg>
2020-05-04 17:59:07 +0800

    Moved Google Maps iOS key to apikey.plist.

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '13.4'
+
+target 'geolocation' do
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+end
--- geolocation/AppDelegate.swift
+++ geolocation/AppDelegate.swift
@@ -14,7 +14,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyBGRdp0zmDQhknq5-NagKqmU0A8DR0fM-I")
+        
+        // Read from plist
+        //
+        // References:
+        // 1. https://stackoverflow.com/a/39453212/1177328
+        // 2. https://makeapppie.com/2016/02/11/how-to-use-property-lists-plist-in-swift
+        var propertyListFormat =  PropertyListSerialization.PropertyListFormat.xml //Format of the Property List.
+        var plistData: [String: AnyObject] = [:] //Our data
+        let plistPath: String? = Bundle.main.path(forResource: "apikey", ofType: "plist")!
+        let plistXML = FileManager.default.contents(atPath: plistPath!)!
+        do {
+            // Convert the data to a dictionary and handle errors.
+            plistData = try PropertyListSerialization.propertyList(from: plistXML, options: .mutableContainersAndLeaves, format: &propertyListFormat) as! [String:AnyObject]
+            
+            GMSServices.provideAPIKey(plistData["GoogleMapsiOSAPIKey"] as! String)
+
+        } catch {
+            print("Error reading plist: \(error), format: \(propertyListFormat)")
+        }
         
         return true
     }
